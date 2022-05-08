f374347e985ee1e87ea9000124a378344b0e3e21
Liam Mandeville <liamman1990@gmail.com>
2022-05-07 19:14:52 -0700

    finished reading/tasks

--- js/main.js
+++ js/main.js
@@ -43,15 +43,68 @@ function checkAge(age) {
     if (age >= 18) {
         return true;
     } else {
-        return confirm('Do you have permission from your parentss ?');
+        return confirm('Do you have permission from your parents ?');
     }
 }
 
 // let age = prompt('How old are you ?', 18);
 
-if (checkAge(age)) {
-    alert('Access Granted');
-} else {
-    alert('Access Denied');
+//  Task 2.
+function isOfAge(age) {
+    if (age <= 18) {
+        return true;
+    } else {
+        return confirm('Did your parents allow you ?');
+    }
 }
 
+// Turnery example
+function isOfAge(age) {
+    return (age >= 18) ? true : confirm('Did your parents allow you ?');
+}
+
+// let ofAge = console.log(isOfAge(15));
+
+// || or example
+function isOfAge(age) {
+    return (age >=18) || confirm('Did your parents allow you ?');
+}
+
+// let ofAge = console.log(isOfAge(15));
+
+// Task 3.
+function returnMinNumber(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+// let min = console.log(returnMinNumber(4, 5));
+
+// Same but using the turnery operator
+function minNumber(a, b) {
+    return (a < b) ? a : b;
+}
+
+// let min = console.log(minNumber(6, 9));
+
+// Task 4
+function pow(x, n) {
+    let result = x;
+
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+    return result;
+}
+
+// let x = prompt(`What is X ?`);
+// let n = prompt(`What is N ?`);
+
+if (n < 1) {
+    alert(`Power ${n} is not supported, use a positive integer.`);
+} else {
+    alert(pow(x, n));
+}
